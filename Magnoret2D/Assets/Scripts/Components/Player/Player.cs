ea7a40294696d53319829116d131ac365ed8b58b
Clovis <nachofcz@mail.com>
2019-11-03 15:23:51 -0300

    Minor changes

---
+++ Magnoret2D/Assets/Scripts/Components/Player/Player.cs
@@ -0,0 +1,21 @@
+namespace Player
+{
+    public class Player: IPlayer
+    {
+        #region Builders
+        public Player() { }
+        #endregion
+
+        #region Props
+
+        #endregion
+
+        #region Private Methods
+
+        #endregion
+
+        #region Public Methods
+
+        #endregion
+    }
+}
---
+++ Magnoret2D/Assets/Scripts/Components/Player/PlayerProvider.cs
@@ -0,0 +1,10 @@
+namespace Player
+{
+    public static class PlayerProvider
+    {
+        public static Player GetPlayer()
+        {
+            return new Player();
+        }
+    }
+}
